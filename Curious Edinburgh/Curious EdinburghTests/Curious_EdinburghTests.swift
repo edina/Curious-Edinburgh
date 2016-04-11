ef3a3a95af5ebb1cb2f58c834ebd15a7140371ff
Colin Gormley <colin.gormley@ed.ac.uk>
2016-04-11 15:37:58 +0100

    edina/Curious-Edinburgh/1: Added initial iOS project skeleton

---
+++ Curious Edinburgh/Curious EdinburghTests/Curious_EdinburghTests.swift
@@ -0,0 +1,36 @@
+//
+//  Curious_EdinburghTests.swift
+//  Curious EdinburghTests
+//
+//  Created by Colin Gormley on 11/04/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import XCTest
+@testable import Curious_Edinburgh
+
+class Curious_EdinburghTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
