df7129fba2b88126bb32a74c92b1c22ab8d3edf1
Colin Gormley <colin.gormley@ed.ac.uk>
2016-04-13 10:10:40 +0100

    edina/Curious-Edinburgh/#4: Added constants and API classes

---
+++ Curious Edinburgh/Curious Edinburgh/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Curious Edinburgh
+//
+//  Created by Colin Gormley on 13/04/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import Foundation
+struct Constants {
+ 
+    struct API {
+        static let url = "http://sciencetours.blogs.edina.ac.uk/"
+    }
+
+    struct SegueIDs {
+     
+    }
+
+    struct ImageIdentifiers {
+    
+    }
+    
+}
---
+++ Curious Edinburgh/Curious Edinburgh/CuriousEdinburghAPI.swift
@@ -0,0 +1,25 @@
+//
+//  CuriousEdinburghAPI.swift
+//  Curious Edinburgh
+//
+//  Created by Colin Gormley on 13/04/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import Foundation
+
+
+
+class _CuriousEdinburghAPI {
+    
+    init() {
+  
+    }
+    
+    func posts() {
+        // Return some posts
+    }
+    
+   }
+
+let curiousEdinburghAPI = _CuriousEdinburghAPI()
