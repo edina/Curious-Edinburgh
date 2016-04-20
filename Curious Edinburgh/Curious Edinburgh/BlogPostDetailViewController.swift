c11d2e9a924de9234856a8b479260b91bc2092b8
Ian Fieldhouse <me@ianfieldhouse.com>
2016-04-20 12:18:21 +0100

    edina/Curious-Edinburgh/13: First stab at YouTube video in detail view
    Has same hard coded YouTube id for every detail view at the moment.

--- Curious Edinburgh/Curious Edinburgh/BlogPostDetailViewController.swift
+++ Curious Edinburgh/Curious Edinburgh/BlogPostDetailViewController.swift
@@ -16,6 +16,7 @@ class BlogPostDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var videoView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +27,9 @@ class BlogPostDetailViewController: UIViewController {
             self.webView.loadHTMLString(content, baseURL: nil)
         }
         
-        
+        loadYoutube(videoID: "fuD3Zco0aXs")
+        videoView.scrollView.scrollEnabled = false
+        videoView.scrollView.bounces = false
         
         // Do any additional setup after loading the view.
     }
@@ -34,6 +37,13 @@ class BlogPostDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadYoutube(videoID videoID:String) {
+        guard
+            let youtubeURL = NSURL(string: "https://www.youtube.com/embed/\(videoID)")
+            else { return }
+        videoView.loadRequest( NSURLRequest(URL: youtubeURL) )
     }
     
 
