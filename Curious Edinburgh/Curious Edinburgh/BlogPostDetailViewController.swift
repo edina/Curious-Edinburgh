9ed21eed2e0448c8b93642247dbb02ab907a8d00
Ian Fieldhouse <me@ianfieldhouse.com>
2016-04-20 16:47:20 +0100

    edina/Curious-Edinburgh/13: Remove unnecessary code

--- Curious Edinburgh/Curious Edinburgh/BlogPostDetailViewController.swift
+++ Curious Edinburgh/Curious Edinburgh/BlogPostDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BlogPostDetailViewController: UIViewController, UIWebViewDelegate {
+class BlogPostDetailViewController: UIViewController {
 
     
     var blogPost: BlogPost?
@@ -28,7 +28,7 @@ class BlogPostDetailViewController: UIViewController, UIWebViewDelegate {
             let youtubeElement = "<iframe width=\"100%\" height=\"100px\" src=\"https://www.youtube.com/embed/\(videoId)\" frameborder=\"0\" allowfullscreen></iframe>"
             let content = "\(youtubeElement)\(content)"
             
-            let styleElement = "<style>body { font-family: HelveticaNeue; padding: 0; margin: 0; } p { margin: 1em; } img { width: 40%; height: auto; float: left; padding: 1em 1em 1em 0; }</style>"
+            let styleElement = "<style>body { font-family: HelveticaNeue; padding: 0; margin: 0; } p { margin: 1em; } img { width: 40%; height: auto; float: left; padding: 0 1em 0.25em 0; }</style>"
             let headElement = "<head><title>\(blogPost?.title!)</title>\(styleElement)</head>"
             
             let htmlElement = "<html>\(headElement)<body>\(content)</body></html>"
@@ -36,35 +36,12 @@ class BlogPostDetailViewController: UIViewController, UIWebViewDelegate {
             self.webView.loadHTMLString(htmlElement, baseURL: nil)
         }
         
-        webViewDidFinishLoad(self.webView)
-        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func loadYoutube(videoID videoID:String) {
-        guard
-            let youtubeURL = NSURL(string: "https://www.youtube.com/embed/\(videoID)")
-            else { return }
-        videoView.loadRequest( NSURLRequest(URL: youtubeURL) )
-    }
-    
-    func webViewDidFinishLoad(webView: UIWebView) {
-//        webView.scrollView.scrollEnabled = false;
-//        webView.scrollView.bounces = false
-        
-        var frame = webView.frame;
-        
-        frame.size.height = 1;        // Set the height to a small one.
-        
-        webView.frame = frame;       // Set webView's Frame, forcing the Layout of its embedded scrollView with current Frame's constraints (Width set above).
-        
-        frame.size.height = webView.scrollView.contentSize.height;
-        webView.frame = frame;
     }
     
 
