beda179e0278405cef6917482ee76b6ee36aa661
Ian Fieldhouse <me@ianfieldhouse.com>
2016-07-22 16:41:26 +0100

    edina/Curious-Edinburgh/#46 Add Tours List View Controller

---
+++ Curious Edinburgh/Curious Edinburgh/ToursListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ToursListViewController.swift
+//  Curious Edinburgh
+//
+//  Created by Ian Fieldhouse on 22/07/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import UIKit
+
+
+class ToursListViewController: UIViewController {
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func dismissViewController(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
